f39a2de7198d20df695b2b23b6f47c0e56f534e3
Valkaryne <anix.mig29@gmail.com>
2019-04-01 16:06:56 +0300

    Initialized entry task point

---
+++ AdvancedJava/src/main/java/com/epam/Solution.java
@@ -0,0 +1,8 @@
+package com.epam;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ AdvancedJava/src/main/java/com/epam/impl/GenericNavigator.java
@@ -0,0 +1,15 @@
+package com.epam.impl;
+
+import com.epam.api.*;
+
+public class GenericNavigator implements GpsNavigator {
+    @Override
+    public void readData(String filePath) {
+
+    }
+
+    @Override
+    public Path findPath(String pointA, String pointB) {
+        return null;
+    }
+}
