7ba947b53236d866bad9a453a87fb8139c2ad958
Valkaryne <anix.mig29@gmail.com>
2019-04-01 15:35:04 +0300

    Created base with gradle

--- AdvancedJava/src/com/epam/calculations/Calculator.java
+++
@@ -1,5 +0,0 @@
-package com.epam.calculations;
-
-public interface Calculator<T> {
-    int calculate(T n1, T n2);
-}
--- AdvancedJava/src/com/epam/utils/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Put all your implementation into this package.
- */
-package com.epam.utils;
