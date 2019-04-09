7f2a8b7ba74bc7782f41c90353cef66ed9258af3
Valkaryne <anix.mig29@gmail.com>
2019-04-09 21:49:57 +0300

    [TASK4-1] Made it able to save loaded image state

--- tsk04_Overview/app/src/main/java/com/epam/valkaryne/imagepicker/MainActivity.kt
+++ tsk04_Overview/app/src/main/java/com/epam/valkaryne/imagepicker/MainActivity.kt
@@ -2,6 +2,7 @@ package com.epam.valkaryne.imagepicker
 
 import android.app.Activity
 import android.content.Intent
+import android.graphics.Bitmap
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
@@ -14,9 +15,16 @@ class MainActivity : AppCompatActivity() {
         private const val IMAGE_PICKED_REQUEST = 1101
     }
 
+    private var bitmap: Bitmap? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState != null) {
+            bitmap = savedInstanceState.getParcelable("image") as Bitmap
+            iv_image.setImageBitmap(bitmap)
+        }
 
         btn_gallery.setOnClickListener { navigateToGallery() }
     }
@@ -29,6 +37,11 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        outState?.putParcelable("image", bitmap)
+    }
+
     private fun navigateToGallery() {
         val galleryIntent = Intent()
         galleryIntent.action = Intent.ACTION_PICK
@@ -39,7 +52,7 @@ class MainActivity : AppCompatActivity() {
     private fun resolveImageData(resultCode: Int, data: Intent?) {
         if (resultCode == Activity.RESULT_OK && data != null) {
             val pickedImage = data.data
-            val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, pickedImage)
+            bitmap = MediaStore.Images.Media.getBitmap(contentResolver, pickedImage)
             iv_image.setImageBitmap(bitmap)
         } else {
             Toast.makeText(applicationContext, "Error while opening image", Toast.LENGTH_SHORT).show()
