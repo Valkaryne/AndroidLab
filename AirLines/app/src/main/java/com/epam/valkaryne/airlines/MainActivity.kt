f4d64c7cc583d31d3d0dd57661b859755c03a50c
Valkaryne <anix.mig29@gmail.com>
2019-04-18 16:05:21 +0300

    [TASK6-2] Added layout for constraint and non-constraint types

--- AirLines/app/src/main/java/com/epam/valkaryne/airlines/MainActivity.kt
+++ AirLines/app/src/main/java/com/epam/valkaryne/airlines/MainActivity.kt
@@ -2,11 +2,22 @@ package com.epam.valkaryne.airlines
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.Fragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_show_constraint.setOnClickListener { replaceFragment(DepartureConstraintFragment()) }
+        btn_show_nonconstraint.setOnClickListener { replaceFragment(DepartureNonconstraintFragment()) }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragmentContainer, fragment)
+        transaction.commit()
     }
 }
