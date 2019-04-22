ea352402ae004f9c08e3e9fe6a2041592c4fd84e
Valkaryne <anix.mig29@gmail.com>
2019-04-22 12:23:32 +0300

    [TASK6-12] A little refactoring of _nonconstraint.xml

--- AirLines/app/src/main/java/com/epam/valkaryne/airlines/MainActivity.kt
+++ AirLines/app/src/main/java/com/epam/valkaryne/airlines/MainActivity.kt
@@ -3,7 +3,6 @@ package com.epam.valkaryne.airlines
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.util.Log
 import android.widget.Button
 
 /**
