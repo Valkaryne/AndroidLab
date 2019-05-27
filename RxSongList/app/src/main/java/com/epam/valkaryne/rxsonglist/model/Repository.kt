bb35192f60da1ea2c1c55a6b52d4539f88dc07c5
valkaryne <de20us12ex>
2019-05-27 21:57:01 +0300

    [TASK14-2] Implemented simple repository

---
+++ RxSongList/app/src/main/java/com/epam/valkaryne/rxsonglist/model/Repository.kt
@@ -0,0 +1,57 @@
+package com.epam.valkaryne.rxsonglist.model
+
+import com.epam.valkaryne.rxsonglist.R
+import io.reactivex.Observable
+import java.util.concurrent.TimeUnit
+
+class Repository {
+
+    private val titles = arrayListOf(
+        "The Night King",
+        "Shattered",
+        "Khazad Dum",
+        "Once My Light",
+        "Stellar Tombs",
+        "The Blood of Ardennes",
+        "Through the Mists of Oblivion"
+    )
+
+    private val artists = arrayListOf(
+        "Ramin Djawadi",
+        "Delain",
+        "Summoning",
+        "Sirenia",
+        "Draconian",
+        "Unshine",
+        "Atargatis"
+    )
+
+    private val lengths = arrayListOf(
+        "8:50",
+        "4:19",
+        "10:57",
+        "7:20",
+        "6:02",
+        "4:35",
+        "6:39"
+    )
+
+    private val imgResources = arrayListOf(
+        R.drawable.got,
+        R.drawable.delain,
+        R.drawable.summoning,
+        R.drawable.sirenia,
+        R.drawable.draconian,
+        R.drawable.unshine,
+        R.drawable.atargatis
+    )
+
+    fun getItems() =
+        Observable
+            .range(0, 7)
+            .concatMap { i ->
+                Observable.just(
+                    Song(titles[i], artists[i], lengths[i], imgResources[i])
+                ).delay(5, TimeUnit.SECONDS)
+            }
+}
---
+++ RxSongList/app/src/main/java/com/epam/valkaryne/rxsonglist/model/Song.kt
@@ -0,0 +1,8 @@
+package com.epam.valkaryne.rxsonglist.model
+
+data class Song(
+    val title: String,
+    val artist: String,
+    val length: String,
+    val imgRes: Int
+)
